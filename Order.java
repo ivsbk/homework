d14a28c92e4282336708b0dcb043c49de2b3dd67
ivsbk <103572344+ivsbk@users.noreply.github.com>
2022-06-08 19:27:40 +0300

    Add files via upload

---
+++ Order.java
@@ -0,0 +1,32 @@
+package Orders;
+
+import Address.Address;
+import Products.Product;
+
+import java.io.Serializable;
+import java.util.List;
+
+
+public class Order{
+    public static int id;
+    public List<Product> productList;
+    public Address address;
+
+    public Order() {
+    }
+
+    @Override
+    public String toString() {
+        return "Order{" + "id=" + id +
+                ", productList=" + productList +
+                ", address=" + address +
+                '}';
+    }
+
+    public Order(List<Product> productList, Address address) {
+        id++;
+        this.productList = productList;
+        this.address = address;
+    }
+
+}
